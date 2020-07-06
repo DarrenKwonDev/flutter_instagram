0236151964b40e215e8b7be253f8821aba7aec82
DarrenKwonDev <anatomy1545@gmail.com>
2020-07-06 21:46:50 +0900

    add richtext

--- lib/screens/feed_page.dart
+++ lib/screens/feed_page.dart
@@ -1,3 +1,4 @@
+import 'package:com/constants/size.dart';
 import 'package:com/utils/profile_img_path.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -40,54 +41,121 @@ class _FeedPageState extends State<FeedPage> {
       body: ListView.builder(
           itemCount: 15,
           itemBuilder: (BuildContext context, int index) {
-            return _postItem(index);
+            return _postItem(index, context);
           }),
     );
   }
 
-  Column _postItem(int index) {
+  Column _postItem(int index, context) {
     return Column(
-            children: <Widget>[
-              _postHeader("username $index"),
-              _postImage(index),
-            ],
-          );
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _postHeader("username $index"),
+        _postImage(index),
+        _postActions(),
+        _postLikes(),
+        _postCaption(context, index)
+      ],
+    );
+  }
+
+  Padding _postCaption(context, int index) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: common_gap),
+        child: RichText(
+            text: TextSpan(
+                style: DefaultTextStyle.of(context).style,
+                children: <TextSpan>[
+              TextSpan(
+                text: "username $index",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                  text: ""
+              ),
+              TextSpan(
+                text: "I like my new laptop sooooooooooooooooooo much",
+              ),
+            ])),
+      );
+  }
+
+  Padding _postLikes() {
+    return Padding(
+      padding: const EdgeInsets.only(left: common_gap),
+      child: Text("80likes", style: TextStyle(fontWeight: FontWeight.bold)),
+    );
+  }
+
+  Row _postActions() {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/bookmark.png'),
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/comment.png'),
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/direct_message.png'),
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+        Spacer(),
+        IconButton(
+          icon: ImageIcon(
+            AssetImage('assets/heart_selected.png'),
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 
   Row _postHeader(String username) {
     return Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: CircleAvatar(
-                    radius: 16,
-                    backgroundImage: CachedNetworkImageProvider(
-                        getProfileImgPath(username)
-                    ),
-                  ),
-                ),
-                Expanded(child: Text(username)),
-                IconButton(
-                  icon: Icon(Icons.more, color: Colors.black87), onPressed: () {  },
-                )
-              ],
-            );
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: CircleAvatar(
+            radius: profile_radius,
+            backgroundImage:
+                CachedNetworkImageProvider(getProfileImgPath(username)),
+          ),
+        ),
+        Expanded(child: Text(username)),
+        IconButton(
+          icon: Icon(Icons.more, color: Colors.black87),
+          onPressed: () {},
+        )
+      ],
+    );
   }
 
   CachedNetworkImage _postImage(int index) {
     return CachedNetworkImage(
-                imageUrl: "https://picsum.photos/id/$index/200/200",
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-                imageBuilder: (context, imageProvider) => AspectRatio(
-                  aspectRatio: 1,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: imageProvider, fit: BoxFit.cover),
-                    ),
-                  ),
-                ),
-              );
+      imageUrl: "https://picsum.photos/id/$index/200/200",
+      placeholder: (context, url) => CircularProgressIndicator(),
+      errorWidget: (context, url, error) => Icon(Icons.error),
+      imageBuilder: (context, imageProvider) => AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+          ),
+        ),
+      ),
+    );
   }
 }
