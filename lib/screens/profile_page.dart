91020cce2fbd3690512bb0bf9d2187511ad8e0bb
DarrenKwonDev <anatomy1545@gmail.com>
2020-07-07 18:05:51 +0900

    slide bar

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:com/constants/size.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -17,17 +18,10 @@ class _ProfilePageState extends State<ProfilePage> {
     menuWidth = _size.width / 1.5;
 
     return Scaffold(
-      appBar: AppBar(),
       body: Stack(
         children: <Widget>[_sideMenu(), _profile()],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _menuOpened = !_menuOpened;
-          });
-        },
-      ),
+
     );
   }
 
@@ -35,19 +29,41 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget _sideMenu() {
     return AnimatedContainer(
       duration: Duration(milliseconds: duration),
-      color: Colors.grey,
+      color: Colors.grey[200],
       curve: Curves.easeInOut,
+      // 메뉴가 열리면 화면의 가로 길이에서 메뉴의 크기 만큼 뺀 공간을 주어서 보이게 만듭니다.
+      // 열리지 않았다면 현재 화면 길이 만큼 _profile이 차지하고 있으니 0을 주어도 무방합니다
       transform: Matrix4.translationValues(
-          _menuOpened ? _size.width - menuWidth : _size.width, 0, 0),
+          _menuOpened ? _size.width - menuWidth : 0, 0, 0),
     );
   }
 
   Widget _profile() {
     return AnimatedContainer(
       duration: Duration(milliseconds: duration),
-      color: Colors.redAccent,
+      color: Colors.white,
       curve: Curves.easeInOut,
+      // 메뉴가 열리면 메뉴의 길이 만큼 왼쪽으로 밀어냅니다. 안 열리면 굳이 애니메이션을 줄 필요없으니 0
       transform: Matrix4.translationValues(_menuOpened ? -menuWidth : 0, 0, 0),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.only(left: common_gap),
+                  child: Text("This is Username"),
+                )),
+                IconButton(icon: Icon(Icons.menu), onPressed: () {
+                  setState(() {
+                    _menuOpened = !_menuOpened;
+                  });
+                },)
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
